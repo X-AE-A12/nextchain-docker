e153306d54459b80ae621e145b02fe9ce7303149
nobody <timo_vvliet@live.nl>
2020-05-27 14:57:22 +0200

    fixed path to next.conf
    The path to the original nextcoin.conf was broken. It was missing a dir + the wallet refers to next as nextcoin, not as 'next'

--- 3.5.0/docker-entrypoint.sh
+++ 3.5.0/docker-entrypoint.sh
@@ -4,22 +4,31 @@ set -e
 if [[ "$1" == "next-cli" || "$1" == "nextd" ]]; then
 	mkdir -p "$NEXT_DATA"
 
-	cat <<-EOF > "$NEXT_DATA/next.conf"
-	printtoconsole=1
-	rpcallowip=::/0
-	rpcusername=${NEXT_RPC_USERNAME:-username}
-	rpcpassword=${NEXT_RPC_PASSWORD:-password}
-	EOF
-	chown next:next "$NEXT_DATA/next.conf"
+	if [[ ! -s "$NEXT_DATA/nextcoin.conf" ]]; then
+		cat <<-EOF > "$NEXT_DATA/nextcoin.conf"
+		daemon=0
+		printtoconsole=0
+		server=1
+		rpcallowip=::/0
+		rpcuser=${NEXT_RPC_USERNAME:-user}
+		rpcpassword=${NEXT_RPC_PASSWORD:-password}
+		rpcbind=0.0.0.0
+		whitelist=0.0.0.0/0
+		EOF
+		chown nextcoin:nextcoin "$NEXT_DATA/nextcoin.conf"
+	fi
 
 	# ensure correct ownership and linking of data directory
 	# we do not update group ownership here, in case users want to mount
 	# a host directory and still retain access to it
-	chown -R next "$NEXT_DATA"
-	ln -sfn "$NEXT_DATA" /home/next/.next
-	chown -h next:next /home/next/.next
+	chown -R nextcoin "$NEXT_DATA"
+	ln -sfn "$NEXT_DATA" /home/nextcoin/.next
+	chown -h nextcoin:nextcoin /home/nextcoin/.next
 
-	exec gosu next "$@"
+	echo "These are the contents of nextcoin.conf: "
+	cat /home/nextcoin/.next/nextcoin.conf
+
+	exec gosu nextcoin "$@"
 else
 	exec "$@"
 fi
